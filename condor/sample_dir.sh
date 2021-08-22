5ae00068011ea7498bd658020d88536622017a5e
renqipan <renqiph@163.com>
2021-08-22 17:16:03 +0800

    update condor

---
+++ condor/sample_dir.sh
@@ -0,0 +1,17 @@
+input="dataset.txt"
+i=0
+for dataset in `cat $input`
+do 
+	echo $dataset
+	var="Chunk$i"
+	rm -rf $var
+    mkdir $var
+    cd $var
+	mkdir condor_out
+    temp=${dataset%%-pythia8*}
+    process=${temp:1}
+    #dasgoclient --query "file dataset=$dataset" > ${process}.txt
+    dasgoclient -query="file dataset=$dataset" > ${process}.txt
+	cd ../
+	let i=i+1
+done
