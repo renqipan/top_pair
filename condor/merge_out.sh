41716113b91476e8e0019c5a0dfa65ea01f2e195
renqipan <renqiph@163.com>
2021-10-24 18:40:24 +0800

    update merge_out.sh

--- condor/merge_out.sh
+++ condor/merge_out.sh
@@ -1,20 +1,21 @@
-#written by Renqi Pan in August 30,2021
-rm -rf out_put
-mkdir  out_put
+dir=output
+rm -rf $dir
+mkdir $dir
 input="dataset.txt"
 i=0
 for process in `cat $input`
 do 
     var=Chunk$i
-    cp $var/condor_out/run.out out_put/${var}_run.out 2>/dev/null
-    cp $var/condor_out/run.err out_put/${var}_run.err 2>/dev/null
-    cp $var/condor_out/run.log out_put/${var}_run.log 2>/dev/null
-#    mv $var/new*.root out_put 2>/dev/null || :
+    cp $var/condor_out/run.out $dir/${var}_run.out 2>/dev/null
+    cp $var/condor_out/run.err $dir/${var}_run.err 2>/dev/null
+    cp $var/condor_out/run.log $dir/${var}_run.log 2>/dev/null
+    mv $var/new*.root $dir 2>/dev/null || :
 #    rm -rf $var
     let i=i+1
 done
-cd out_put
-cat *_run.out > all.out
-cat *_run.err > all.err
-cat *_run.log > all.log
+cd $dir
+cat *_run.out > all2.out
+cat *_run.err > all2.err
+cat *_run.log > all2.log
 rm *run*
+
